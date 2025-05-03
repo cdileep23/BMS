fda80c95bbafd29436f04f0ed29fe62d56582ac1
dileep <cdileepkumar22@gmail.com>
2025-05-03 12:50:24 +0530

    finalush

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_DB_URL);
-
-    console.log("Server connected to Database");
-  } catch (error) {
-    console.log(error);
-  }
+  await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("✅ MongoDB connected");
 };
+
 export default connectDB;
